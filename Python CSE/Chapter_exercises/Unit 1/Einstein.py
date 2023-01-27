68b6fa6b1e37afe5667bb0b0ac780e3e6956f74b
Tyler <tyleresch29@gmail.com>
2023-01-27 03:20:00 -0500

    hey new commit

---
+++ Python CSE/Chapter_exercises/Unit 1/Einstein.py
@@ -0,0 +1,9 @@
+m_str = input('Input m: \n')  # do not change this line
+# change m_str to a float
+# remember you need c
+# e =  
+m = float(m_str)
+c = 300000000
+e = m * c ** 2
+print ("e =", e)  # do not change this line
+
