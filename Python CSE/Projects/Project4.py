cfb0e5cd50517870d09945ac47070fde57f57fdd
Tyleresch <113705714+Tyleresch@users.noreply.github.com>
2023-02-02 19:51:36 -0500

    hey

---
+++ Python CSE/Projects/Project4.py
@@ -0,0 +1,83 @@
+''' Insert heading comments here.'''
+
+
+MENU = '''\nPlease choose one of the options below:
+             A. Convert a decimal number to another base system         
+             B. Convert decimal number from another base.
+             C. Convert from one representation system to another.
+             E. Encode an image with a text.
+             D. Decode an image.
+             M. Display the menu of options.
+             X. Exit from the program.'''
+    
+def numtobase( N, B ):
+    '''Insert docstring here.'''
+    bit = ''
+    Quotient = 0
+    while N != 0:
+        Quotient = N // B 
+        Remainder = N % B
+        N = Quotient
+        Remainder = str(Remainder)
+        bit = Remainder + bit
+    if len(bit) % 8 == 0: 
+        return (bit)
+    else:
+        while len(bit) % 8 != 0:
+            bit = str('0') + bit 
+        return (bit)
+
+
+
+    pass  # insert your code here
+
+def basetonum( S, B ):
+    '''Insert docstring here.'''
+    Total_number = int(0)
+    nums = len(S) - 1
+    for i in range(len(S)):
+        Total_number += int(S[i]) * B ** nums
+        nums = nums -1 
+    return(Total_number)
+
+
+        
+    pass  # insert your code here
+
+def basetobase(B1,B2,s_in_B1):
+    '''Insert docstring here.'''
+    nums_1 = basetonum(s_in_B1, B1)
+    return(numtobase(nums_1, B2))
+    pass  # insert your code here
+
+ 
+def encode_image(image,text,N):
+    '''Insert docstring here.'''
+        
+    pass  # insert your code here
+
+
+def decode_image(sego,N):
+    '''Insert docstring here.'''
+    pass  # insert your code here         
+
+def main():
+    BANNER = '''
+               A long time ago in a galaxy far, far away...   
+              A terrible civil war burns throughout the galaxy.      
+  ~~ Your mission: Tatooine planet is under attack from stormtroopers,
+                   and there is only one line of defense remaining        
+                   It is up to you to stop the invasion and save the planet~~
+    '''
+
+    print(BANNER)
+    
+    pass  # insert your code here
+
+# These two lines allow this program to be imported into other code
+# such as our function tests code allowing other functions to be run
+# and tested without 'main' running.  However, when this program is
+# run alone, 'main' will execute.
+#DO NOT CHANGE THESE 2 lines  
+if __name__ == '__main__': 
+    main()
